00d1ade857d0834a53b686908bfa87dda98c2a5e
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-07-31 11:11:12 +0300

    added route for mpesa

--- src/AppWithAuth.js
+++ src/AppWithAuth.js
@@ -1,15 +1,27 @@
 import React, { Suspense } from 'react';
-import CheckoutPage from './pages/CheckoutPage';
-
+import { Routes, Route } from 'react-router-dom';
 
 const AuthProvider = React.lazy(() => import('./AuthProvider'));
 const App = React.lazy(() => import('./App'));
+const CheckoutPage = React.lazy(() => import('./CheckoutPage')); 
 
 const AppWithAuth = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <AuthProvider>
-    <Route path="/checkout/:orderId" element={<CheckoutPage />} />
-      <App />
+      <Routes>
+        {/* Main app routes */}
+        <Route path="/*" element={<App />} />
+        
+        {/* Protected payment route */}
+        <Route 
+          path="/checkout/:orderId" 
+          element={
+            <React.Suspense fallback={<div>Loading payment...</div>}>
+              <CheckoutPage />
+            </React.Suspense>
+          } 
+        />
+      </Routes>
     </AuthProvider>
   </Suspense>
 );
