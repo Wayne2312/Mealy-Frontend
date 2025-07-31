26999fafeeb46a83d645111cb30f3c15f3558dd1
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-07-31 20:50:42 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useAuth } from './AuthProvider';
+import Header from './Header';
+import LoginForm from './LoginForm';
+import CustomerDashboard from './CustomerDashboard';
+import AdminDashboard from './AdminDashboard';
+import Home from './Home'; 
+import './App.css';
+
+const App = () => {
+  const { user } = useAuth();
+  if (!user) {
+    return <Home />;
+  }
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      {user.role === 'admin' ? <AdminDashboard /> : <CustomerDashboard />}
+    </div>
+  );
+};
+
+export default App;
