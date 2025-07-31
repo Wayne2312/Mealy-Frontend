bd80146076e7de9ec00302d573f8b9e9b8e1423e
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-07-31 20:55:40 +0300

    Update AppWithAuth.jsx
    Routes added

--- src/AppWithAuth.jsx
+++ src/AppWithAuth.jsx
@@ -1,15 +1,29 @@
 import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-// Must default export a valid component
 const AuthProvider = React.lazy(() => import('./AuthProvider'));
 const App = React.lazy(() => import('./App'));
+const CheckoutPage = React.lazy(() => import('./CheckoutPage')); 
 
 const AppWithAuth = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route 
+          path="/checkout/:orderId" 
+          element={
+            <React.Suspense fallback={<div>Loading payment...</div>}>
+              <CheckoutPage />
+            </React.Suspense>
+          } 
+        />
+      </Routes>
+      </BrowserRouter>
     </AuthProvider>
   </Suspense>
 );
 
-export default AppWithAuth;
+export default AppWithAuth
